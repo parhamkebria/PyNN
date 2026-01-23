310127abb89c641154df6cc7473af54b0c9d4ceb
Parham Kebria <41775591+parhamkebria@users.noreply.github.com>
2026-01-22 23:27:31 -0500

    Implement model training and testing in example.py
    Added model building, training, and testing code.

--- example.py
+++ example.py
@@ -3,6 +3,7 @@ import random
 
 random.seed(42)
 
+# build the model
 model = NeuralNetwork(
     layers=[
         Dense(2, 8),
@@ -15,4 +16,10 @@ model = NeuralNetwork(
     lr=0.1
 )
 
+# train the model
 model.train(X, Y, epochs=2000, batch_size=2)
+
+# test the model
+print("\nPredictions:")
+for x in X:
+    print(x, model.forward(x))
