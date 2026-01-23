d35bd7bdab2c45fd7cb1f1f7b9cbfa15a77f5729
Parham Kebria <41775591+parhamkebria@users.noreply.github.com>
2026-01-22 23:15:13 -0500

    Add Layer class for neural network models

---
+++ pynn.py
@@ -0,0 +1,14 @@
+import math
+import random
+
+# Layer object to be used when building NN models
+class Layer:
+    def forward(self, inputs):
+        raise NotImplementedError
+
+    def backward(self, grad_output):
+        raise NotImplementedError
+
+    def update(self, lr):
+        pass
+
